bdbe4f7b4b36a806366df742829a4cefa14e085d
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-01-19 00:06:56 +0500

    Update the bootsrap4 css and js files

--- Medical-One/Bootstrap 4/js/script.js
+++ Medical-One/Bootstrap 4/js/script.js
@@ -71,28 +71,58 @@ $(function () {
         return false;
     });
 });
+$(function () {
+    var showChar = 178;
+    var moretext = "Read More";
+    var lesstext = "Read Less";
+    $('.comments-spaces').each(function () {
+        var content = $(this).html();
+        if (content.length > showChar) {
+            var show_content = content.substr(0, showChar);
+            var hide_content = content.substr(showChar, content.length - showChar);
+            var html = show_content + '<span class="remaining-contents"><span>' + hide_content +
+                '</span><a href="" class="morelinkss btn btn-more-load link-style mt-45 btn-r-0" style="display:block;" >' + moretext + '</a>'
+                + '</span>';
+            $(this).html(html);
+        }
+    });
+
+    $(".morelinkss").click(function () {
+
+        if ($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+});
 
 /*===================== Load More Images ======================*/
-//$(function () {
-//    $('a').smoothScroll();
-//});
+$(function () {
+    $('a').smoothScroll();
+});
 /*======================= Nav Active Class =======================*/
 
 $(function () {
-    $('.navbar-nav li a').on('click',
+    $('.nav-link').on('click',
         function () {
-            $('.navbar-nav li a').addClass('nav-active');
-            $(this).removeClass("nav-active");
+            $('.nav-link').removeClass('nav-active');
+            $(this).addClass('nav-active');
         });
 });
 /*======================= Owl Carousel =======================*/
-$(document).ready(function () {
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > 10) {
-            $('.navbar-nav li a').addClass('text-white');
+//$(document).ready(function () {
+//    $(window).scroll(function () {
+//        if ($(window).scrollTop() > 10) {
+//            $('.navbar-nav li a').addClass('text-white');
 
-        } else {
-            $('.navbar-nav li a').removeClass('text-white');
-        }
-    });
-});
+//        } else {
+//            $('.navbar-nav li a').removeClass('text-white');
+//        }
+//    });
+//});
